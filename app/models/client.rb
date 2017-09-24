f41873d31d5b2c1e10f6b6cf50a7794f05eb13bd
Dylan Yang <Dylan@Dylans-Air.home>
2017-09-24 15:34:22 -0400

    Add uniquness validation to user. Add presence, numericality and uniqueness validations to  clients.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,8 @@
 class Client < ApplicationRecord
+  validates :company_name, :value, :user_id, presence: true
+  validates :value, numericality: { only_integer: true }
+  validates :company_name, uniqueness: true
+
   belongs_to :user
   has_many :comments
   has_many :contacts
