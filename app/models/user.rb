1fbbb8a6b1ad0cf9bcc771cbb446d70342f0a8a5
Dylan Yang <Dylan@Dylans-Air.home>
2017-09-24 15:29:19 -0400

    Create user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :company, :email, :password_digest, presence: true
-  validates :email, confirmation: true
-  validates :email_confirmation, presence: true
+  validates :password_digest, length: { minimum: 6 }
 
   has_many :clients
   has_secure_password
