8b2c2b5b75564a96c5af0733e100706dc27bf6d9
Dylan Yang <Dylan@Dylans-Air.home>
2017-09-24 15:37:56 -0400

    Add presence and numericality validations to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
+  validates :name, :role, :email, :phone, :decision_maker, :client, presence: true
+  validates :phone, numericality: { only_integer: true }
+
   belongs_to :client
 end
