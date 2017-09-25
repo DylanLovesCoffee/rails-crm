c4bf7c06159d51d6b76e5cbbbad1bbb4a6f08594
Dylan Yang <Dylan@Dylans-Air.home>
2017-09-24 21:33:06 -0400

    Add edit view for users. Use flash for errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      @errors = ["Not a valid email or password."]
+      flash[:error] = 'Not a valid email or password.'
       render 'login'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,34 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
+      flash[:success] = "Thanks for signing up, #{@user.first_name}!"
       redirect_to @user
     else
-      @errors = @user.errors.full_messages
+      @user.errors.full_messages.each do |error|
+        flash[:error] = error
+      end
       render 'new'
     end
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by(id: session[:user_id])
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def update
+    @user = User.find_by(id: session[:user_id])
+
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile Updated'
+      redirect_to @user
+    else
+      @errors = ['Unable to update profile.']
+      render 'edit'
+    end
   end
 
   private
