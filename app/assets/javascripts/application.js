adecac995ce9f7d2b8361d23c3cc504f6aec57ea
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-10-04 23:00:24 -0400

    Hide contact on opening edit form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -93,12 +93,13 @@ $(document).ready(function(){
 
   $(".contact-list-item").on("click", ".edit-contact", function(e){
     e.preventDefault();
-    var $contactEdit = $(this)
+    var $contactEdit = $(this);
     $.ajax({
       url: $contactEdit.attr('href'),
       method: $contactEdit.attr('method')
     }).done(function(response){
-      $contactEdit.parents(".contact-list-item").append(response)
+      $contactEdit.parents(".contact-list-item").append(response);
+      $contactEdit.parents(".contact-row").hide();
     })
   })
 })
